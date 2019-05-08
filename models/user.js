ecabe5f7227512ff3b2387ccb9786397469782bd
Artur Ch <chmarus@gmail.com>
2019-05-08 12:30:51 +0200

    Add role attribute to user

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    active: DataTypes.BOOLEAN
+    active: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
   }, {})
   User.associate = function (models) {
     // associations can be defined here
