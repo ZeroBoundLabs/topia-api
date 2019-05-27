8cdded4e0f0cada465918ebe139d645f6d399584
Artur Ch <chmarus@gmail.com>
2019-05-27 10:26:10 +0200

    Fix typo at migration

--- migrations/20190524111811-create-newsletter-email.js
+++ migrations/20190524111811-create-newsletter-email.js
@@ -22,6 +22,6 @@ module.exports = {
     })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('NewsletterEmails')
+    return queryInterface.dropTable('newsletter_emails')
   }
 }
