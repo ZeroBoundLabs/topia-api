21d30766f8467884b029396c42a8e1110cc20907
Artur Ch <chmarus@gmail.com>
2019-05-30 15:29:00 +0200

    Fix table name

--- seeders/20190503210804-demo-organisation_users.js
+++ seeders/20190503210804-demo-organisation_users.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
-      'organisation_user',
+      'organisation_users',
       [
         {
           role: 'admin',
