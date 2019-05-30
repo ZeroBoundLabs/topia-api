9abb99be04de1690c22eea20f8820671dc1578ca
Artur Ch <chmarus@gmail.com>
2019-05-30 15:25:21 +0200

    Add activation token to User model

--- models/user.js
+++ models/user.js
@@ -7,10 +7,16 @@ module.exports = (sequelize, DataTypes) => {
     active: DataTypes.BOOLEAN,
     role: DataTypes.STRING,
     avatarFilename: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
+    deletedAt: DataTypes.DATE,
+    activationToken: DataTypes.STRING
   }, {})
   User.associate = function (models) {
     // associations can be defined here
+    User.belongsToMany(models.organisation, {
+      otherKey: 'user_id',
+      foreignKey: 'organisation_id',
+      through: 'organisation_user'
+    })
   }
   return User
 }
